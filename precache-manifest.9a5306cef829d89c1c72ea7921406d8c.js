236ac41cccb82f46c533a5753b7fd848391968b4
theknzz <joaquimpersonalmail@isec.pt>
2020-10-22 16:30:44 +0100

    Updates

---
+++ precache-manifest.9a5306cef829d89c1c72ea7921406d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6938b2126649d7aa05d6f0d4950b6dda",
+    "url": "/emoji-picker/index.html"
+  },
+  {
+    "revision": "2657ac943d30f04bf406",
+    "url": "/emoji-picker/static/css/main.644724a6.chunk.css"
+  },
+  {
+    "revision": "617fa379522cb919aedb",
+    "url": "/emoji-picker/static/js/2.cb804064.chunk.js"
+  },
+  {
+    "revision": "c8947cab6719b49a0d83bbbf212fbf8f",
+    "url": "/emoji-picker/static/js/2.cb804064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2657ac943d30f04bf406",
+    "url": "/emoji-picker/static/js/main.3156211c.chunk.js"
+  },
+  {
+    "revision": "7b9af224badcfefc48e3",
+    "url": "/emoji-picker/static/js/runtime-main.fc0fe8d3.js"
+  }
+]);

b18f62a2e2f9d262e95f145f452ca47d5d4f9c9f
theknzz <joaquimpersonalmail@isec.pt>
2020-10-22 17:34:18 +0100

    Updates

--- precache-manifest.4202c1affc40342205b0361cfbfea45a.js
+++ precache-manifest.4202c1affc40342205b0361cfbfea45a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c41bccfc22b6020dd80bd75cf1c813f",
+    "revision": "61b9ea5ddbc2bb83815e83926a038c18",
     "url": "/emoji-picker/index.html"
   },
   {
-    "revision": "f4c92a7ce0f471b1293d",
+    "revision": "331de635ac24572964d8",
     "url": "/emoji-picker/static/css/main.644724a6.chunk.css"
   },
   {
-    "revision": "617fa379522cb919aedb",
-    "url": "/emoji-picker/static/js/2.cb804064.chunk.js"
+    "revision": "f4ea431bdba6b47af1a4",
+    "url": "/emoji-picker/static/js/2.6734cc43.chunk.js"
   },
   {
     "revision": "c8947cab6719b49a0d83bbbf212fbf8f",
-    "url": "/emoji-picker/static/js/2.cb804064.chunk.js.LICENSE.txt"
+    "url": "/emoji-picker/static/js/2.6734cc43.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f4c92a7ce0f471b1293d",
-    "url": "/emoji-picker/static/js/main.d01e5336.chunk.js"
+    "revision": "331de635ac24572964d8",
+    "url": "/emoji-picker/static/js/main.d57f768a.chunk.js"
   },
   {
     "revision": "7b9af224badcfefc48e3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/emoji-picker/precache-manifest.e64c15a368f0c654e55999d9ffffd36c.js"
+  "/emoji-picker/precache-manifest.4202c1affc40342205b0361cfbfea45a.js"
 );
 
 self.addEventListener('message', (event) => {
